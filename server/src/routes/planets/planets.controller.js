61a304b271008cecd25b17e7378859f78ea938e4
David Mihai Matache <davidmihaimatache@gmail.com>
2025-02-06 21:45:54 +0200

    Finished section about databases, working on CI/CD section (Skipped working with REST APIs)

--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,6 +1,7 @@
 const { getAllPlanets } = require("../../models/planets.model");
 
 async function httpGetAllPlanets(req, res) {
+  console.log("HERE");
   return await res.status(200).json(await getAllPlanets());
 }
 
